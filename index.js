b4c3ac5d3808e78588ec0bb1a15f67d6e19641fb
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-08-16 00:27:05 +0300

    js strings

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 let currentUser = 'John Doe';
 let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
-let shortGreeting = `Welcome, ${currentUser[0]}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
